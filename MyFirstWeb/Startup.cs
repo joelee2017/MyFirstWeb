63a67e35f06a46832cab54ba0b4c9c0f7d8095f0
joelee2017 <as955133@gmail.com>
2018-01-31 10:41:21 +0800

    加入專案檔案。

---
+++ MyFirstWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyFirstWeb.Startup))]
+namespace MyFirstWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
