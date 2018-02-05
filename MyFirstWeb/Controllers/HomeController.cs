e6100ec4de28547a11d036af260c09df316340ea
joelee2017 <as955133@gmail.com>
2018-02-05 10:37:26 +0800

    session cache cookie

--- MyFirstWeb/Controllers/HomeController.cs
+++ MyFirstWeb/Controllers/HomeController.cs
@@ -8,14 +8,29 @@ namespace MyFirstWeb.Controllers
 {
     public class HomeController : Controller
     {
+        string SessionKey = "SessinoKey";
+        string cacheKey = "cacheKey";
+        string CookieName = "CookieName";
+        string CookieKey = "CookieKey";
+
         public ActionResult Index()
         {
+            Session[SessionKey] = "SessinoKey";
+            System.Web.HttpContext.Current.Cache[cacheKey] = "CacheValue";//System.Web.HttpContext 
+            HttpCookie c = new HttpCookie(CookieName);
+            c.Values.Add(CookieKey, "CookieValue");
+            c.Expires = DateTime.Now.AddDays(1); //Cookie保留一天
+            Response.Cookies.Add(c);//回應到Response之中
+
             return View();
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            string SeesionValue = Convert.ToString(Session[SessionKey]);//轉型
+            string cacheValue = Convert.ToString(System.Web.HttpContext.Current.Cache[cacheKey]);
+            HttpCookie c = Request.Cookies[CookieName];
+            string CookieValue = Convert.ToString(c.Values[CookieKey]);
 
             return View();
         }
